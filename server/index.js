c07b475373235503906941782bb1febb05f6d11e
Paweł Szostak <szostak_pawel@outlook.com>
2020-02-06 12:49:01 +0100

    Installed CORS package

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 const posts = require('./routes/api/posts')
 app.use('/api/posts', posts);
