44fd5d676d18c866f72a65e946c4cd771070c845
Paweł Szostak <szostak_pawel@outlook.com>
2020-03-19 17:10:57 +0100

    Added vue app

---
+++ vue/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Post />
+  </div>
+</template>
+
+<script>
+import Post from './components/Post.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Post
+  }
+}
+</script>
+
+<style>
+</style>
